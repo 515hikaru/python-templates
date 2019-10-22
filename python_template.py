bc6e701fa76765fb73c83195059b65e089fc4407
515hikaru <12kojima.takahiro@gmail.com>
2019-10-22 22:38:06 +0900

    main file change

---
+++ python_template.py
@@ -0,0 +1,24 @@
+"""
+Awesome package
+"""
+
+
+def fizzbuzz(num):
+    """
+    fizzbuzz logic
+    """
+    ret = ""
+    if num % 3 == 0:
+        ret += "fizz"
+    if num % 5 == 0:
+        ret += "buzz"
+    if ret == "":
+        return str(num)
+    return ret
+
+
+def main():
+    """main routine"""
+    for i in range(100):
+        val = fizzbuzz(i)
+        print(i, val)
